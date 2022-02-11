221fc742b6004e1a8b0ec7adc2e8af296ff02099
MAHI1119 <mahijanu1119@gmail.com>
2022-02-11 15:13:41 +0530

    First commit

--- js/story1.js
+++ js/story1.js
@@ -8,27 +8,13 @@ fetch("json/story.json")
 .then(function(story) {
     document.getElementById("mentee_name1").innerHTML += story[0].name;
     document.getElementById("mentee_name2").innerHTML += story[1].name;
-    document.getElementById("mentee_name3").innerHTML += story[2].name;
-    document.getElementById("mentee_name4").innerHTML += story[3].name;
-    document.getElementById("mentee_name5").innerHTML += story[4].name;
-    document.getElementById("mentee_name6").innerHTML += story[5].name;
-    document.getElementById("mentee_name7").innerHTML += story[6].name;
-
+  
     document.getElementById("mentee_dest1").innerHTML += story[0].design;
     document.getElementById("mentee_dest2").innerHTML += story[1].design;
-    document.getElementById("mentee_dest3").innerHTML += story[2].design;
-    document.getElementById("mentee_dest4").innerHTML += story[3].design;
-    document.getElementById("mentee_dest5").innerHTML += story[4].design;
-    document.getElementById("mentee_dest6").innerHTML += story[5].design;
-    document.getElementById("mentee_dest7").innerHTML += story[6].design;
-
+  
     document.getElementById("mentee_img1").src = story[0].image;
     document.getElementById("mentee_img2").src = story[1].image;
-    document.getElementById("mentee_img3").src = story[2].image;
-    document.getElementById("mentee_img4").src = story[3].image;
-    document.getElementById("mentee_img5").src = story[4].image;
-    document.getElementById("mentee_img6").src = story[5].image;
-    document.getElementById("mentee_img7").src = story[6].image;
+   
 });
 
 function patchname(story) {
@@ -55,29 +41,4 @@ function navigationpage1() {
 function navigationpage2() {
     window.location.href =
         "https://relaxed-albattani-e261f4.netlify.app/story_page2.html?id=2";
-}
-
-function navigationpage3() {
-    window.location.href =
-        "https://relaxed-albattani-e261f4.netlify.app/story_page2.html?id=3";
-}
-
-function navigationpage4() {
-    window.location.href =
-        "https://relaxed-albattani-e261f4.netlify.app/story_page2.html?id=4";
-}
-
-function navigationpage5() {
-    window.location.href =
-        "https://relaxed-albattani-e261f4.netlify.app/story_page2.html?id=5";
-}
-
-function navigationpage6() {
-    window.location.href =
-        "https://relaxed-albattani-e261f4.netlify.app/story_page2.html?id=6";
-}
-
-function navigationpage7() {
-    window.location.href =
-        "https://relaxed-albattani-e261f4.netlify.app/story_page2.html?id=7";
 }
